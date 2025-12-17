bdd55dddfa645071abca798f4d655602d9f9903d
Suma4681 <86455278+Suma4681@users.noreply.github.com>
2025-12-17 16:16:59 -0500

    Delete app/n.py

--- app/n.py
+++
@@ -1,6 +0,0 @@
-import os
-
-csv_path = "/Users/sred/Documents/FINZ_ASSIGNMENT/data/The Winslow_Checking.csv"
-print("CWD:", os.getcwd())
-print("csv_path:", csv_path)
-print("exists:", os.path.exists(csv_path))
